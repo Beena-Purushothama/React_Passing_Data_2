2f3bd3a90cc99bd48d5ab64af304634b037a0fec
Beena-Purushothama <beenajp28@gmail.com>
2019-01-14 22:38:08 +0000

    cosmetic fixes

--- src/ListMovies.js
+++ src/ListMovies.js
@@ -5,12 +5,13 @@ class ListMovies extends Component{
   likedBy = (movieKey) => {
     const {users, movies, profiles} = this.props;
     const movieProfiles = profiles.filter(profile => (profile.favoriteMovieID === movieKey));
-    let userList = [];
-    if(movieProfiles.length > 0){
-      movieProfiles.map((p) => {
-      userList.push(<li key={movieKey+p.userID}>{users[p.userID].name}</li>);
-      });
-      return (userList);
+    if(movieProfiles && movieProfiles.length > 0){
+      const listofItems  = movieProfiles.map((p) => (
+      <li key={movieKey+p.userID}>
+               {users[p.userID].name}
+      </li>
+      ));
+      return (listofItems);
     }
      return (<li key={movieKey}> None of the current users liked this movie</li>);
   }
